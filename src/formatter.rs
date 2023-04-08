291b2896e5609ae2e97f94ad0a68eb8003abef9c
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2023-04-07 23:25:30 -0700

    formatter: allow using "default" terminal color
    The "default" color resets the terminal color to default.

--- src/formatter.rs
+++ src/formatter.rs
@@ -371,6 +371,7 @@ fn rules_from_config(config: &config::Config) -> Result<Rules, config::ConfigErr
 
 fn color_for_name(color_name: &str) -> Option<Color> {
     match color_name {
+        "default" => Some(Color::Reset),
         "black" => Some(Color::Black),
         "red" => Some(Color::DarkRed),
         "green" => Some(Color::DarkGreen),
@@ -861,6 +862,36 @@ mod tests {
         formatter.pop_label().unwrap();
         insta::assert_snapshot!(String::from_utf8(output).unwrap(),
         @"[38;5;1m red before  still red inside  also red afterwards [39m");
+    }
+
+    #[test]
+    fn test_color_formatter_normal_color() {
+        // Normal color resets the color. It can reset only background or only
+        // foreground.
+        let config = config_from_string(
+            r#"
+        colors."outer" = {bg="yellow", fg="blue"}
+        colors."outer default_fg" = "default"
+        colors."outer default_bg" = {bg = "default"}
+        "#,
+        );
+        let mut output: Vec<u8> = vec![];
+        let mut formatter = ColorFormatter::for_config(&mut output, &config).unwrap();
+        formatter.push_label("outer").unwrap();
+        formatter.write_str("Blue on yellow, ").unwrap();
+        formatter.push_label("default_fg").unwrap();
+        formatter.write_str(" default fg, ").unwrap();
+        formatter.pop_label().unwrap();
+        formatter.write_str(" and back.\nBlue on yellow, ").unwrap();
+        formatter.push_label("default_bg").unwrap();
+        formatter.write_str(" default bg, ").unwrap();
+        formatter.pop_label().unwrap();
+        formatter.write_str(" and back.").unwrap();
+        insta::assert_snapshot!(String::from_utf8(output).unwrap(),
+        @r###"
+        [38;5;4m[48;5;3mBlue on yellow, [39m default fg, [38;5;4m and back.[39m[49m
+        [38;5;4m[48;5;3mBlue on yellow, [49m default bg, [48;5;3m and back.
+        "###);
     }
 
     #[test]
