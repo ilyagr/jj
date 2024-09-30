21418744d2dc907493887f47d8bb7e9605bc698e
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-09-30 14:18:14 -0700

    BiasedConflictMarkers implementation

--- lib/src/conflicts.rs
+++ lib/src/conflicts.rs
@@ -236,6 +236,30 @@ pub fn materialize_merge_result<T: AsRef<[u8]>>(
     single_hunk: &Merge<T>,
     output: &mut dyn Write,
 ) -> io::Result<()> {
+    materialize_merge_result_with_options(
+        single_hunk,
+        output,
+        ConflictMaterializationOptions::default(),
+    )
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Default)]
+pub enum ConflictSideBias {
+    #[default]
+    OptimizeDiffSize,
+    Side(usize),
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Default)]
+pub struct ConflictMaterializationOptions {
+    bias: ConflictSideBias,
+}
+
+pub fn materialize_merge_result_with_options(
+    single_hunk: &Merge<BString>,
+    output: &mut dyn Write,
+    options: ConflictMaterializationOptions,
+) -> std::io::Result<()> {
     let merge_result = files::merge(single_hunk);
     match &merge_result {
         MergeResult::Resolved(content) => output.write_all(content),
